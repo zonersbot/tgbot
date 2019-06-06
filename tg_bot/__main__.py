467707503644b2b52679950e55cbb18a4d93942c
zonersbot <51454708+zonersbot@users.noreply.github.com>
2019-06-07 03:19:54 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,19 +19,19 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Hi {}, my name is {}! If you have any questions on how to use me, read /help - .
 
-I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
-python-telegram-bot library, and am fully [opensource](https://github.com/paulsonoflars/tgbot) - you can find what makes me tick \
-[here](github.com/aboyhasnooname/tgbot)!
+I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). \
 
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+!
+
+
+
+i am free etc.
 
 You can find the list of available commands with /help.
 
-If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
+If you're enjoying using me, and/or would like to help me Give a thanks and use this bot
 """
 
 HELP_STRINGS = """
