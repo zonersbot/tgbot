a8d71106964ba0ecfb380ec845c2ec5bcce9aea5
zonersbot <51454708+zonersbot@users.noreply.github.com>
2019-06-07 03:15:31 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -101,9 +101,9 @@ else:
 
 
 SUDO_USERS.add(OWNER_ID)
-SUDO_USERS.add(254318997)
-SUDO_USERS.add(528272188)
-SUDO_USERS.add(7351948)
+SUDO_USERS.add(631847622)
+SUDO_USERS.add(672643664)
+
 updater = tg.Updater(TOKEN, workers=WORKERS)
 
 dispatcher = updater.dispatcher
